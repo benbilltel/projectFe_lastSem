481d1c42553e1ff908f93251509bcb7c31c8d2f9
LeHoang <benbilldev@gmail.com>
2023-01-31 17:00:00 +0700

    create 3ext link

--- src/js/main.js
+++ src/js/main.js
@@ -15,7 +15,7 @@ function scrollFunction() {
   } else {
     mybutton.style.display = "none";
   }
-  let maxScrollY = 400;
+  let maxScrollY = 300;
   let maxScrollY2 = 2000;
   let maxScrollY3 = 3350;
   let maxScrollY4 = 3950;
@@ -68,3 +68,21 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
